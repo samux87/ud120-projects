92dcf4206ec72b3e35875cf784c15c9038993739
Samuele Buosi <samux87@users.noreply.github.com>
2018-10-12 10:10:41 +0200

    Done DT section

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 3 (decision tree) mini-project.
 
-    Use a Decision Tree to identify emails from the Enron corpus by author:    
+    Use a Decision Tree to identify emails from the Enron corpus by author:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -19,13 +19,27 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
+# Modified by Samuele Buosi
 #########################################################
 ### your code goes here ###
+from sklearn.tree import tree
+from sklearn.metrics import accuracy_score
 
+# MAKE IT FASTER!!! using 1% of all data
+#features_train = features_train[:int(len(features_train)/100)]
+#labels_train = labels_train[:int(len(labels_train)/100)]
 
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test, labels_test)
+
+# n features question, Modified selector = SelectPercentile(f_classif, percentile=1)
+# biggest percentile = more complexity
+nfeat=len(features_train[0])
+print('nfeatures= ', nfeat)
+
+acc = accuracy_score(pred, labels_test)
+print("Accuracy:",acc)
+
+print("FINE")
 #########################################################
-
-
