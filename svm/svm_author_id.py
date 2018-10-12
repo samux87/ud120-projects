5c62c3924297604c607e82de236baf6ddf3454a2
Samuele Buosi <samux87@users.noreply.github.com>
2018-10-12 09:51:17 +0200

    Init Commit

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -19,12 +19,45 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
+# Modified by Samuele Buosi
 #########################################################
 ### your code goes here ###
 
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+# MAKE IT FASTER!!! using 1% of all data
+#features_train = features_train[:int(len(features_train)/100)]
+#labels_train = labels_train[:int(len(labels_train)/100)]
+
+# Classifier  playground
+#clf = SVC(kernel="linear") # acc 88%
+#clf = SVC(kernel="rbf") # acc 61%
+clf = SVC(C=10000, kernel="rbf") # acc 89%
+
+clf.fit(features_train,labels_train)
+
+#print("Start prediction")
+pred= clf.predict(features_test)
+
+ten = pred[10]
+ventisei= pred[26]
+cinquanta= pred[50]
+print('pred1= %s, pred2=%s, pred3=%s' %(ten, ventisei, cinquanta))
+a = 0
+b = 0
+for i in pred:
+    if i == 0:
+        a +=1
+    else:
+        b +=1
+
+print("Sara messages=",a,'Chris messages=',b)
+
+#print("Start accuracy test")
+acc = accuracy_score(pred, labels_test)
+print("Accuracy:",acc)
+
+
+print("FINE")
 #########################################################
-
-
--- tools/email_preprocess.py
+++ tools/email_preprocess.py
@@ -1,17 +1,30 @@
+'''
+    Modified by Samuele Buosi for some python 2 to python 3 conversions
+
+
+'''
+
 #!/usr/bin/python
 
 import pickle
-import cPickle
+#import cPickle
+import _pickle as cPickle
 import numpy
 
 from sklearn import cross_validation
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import SelectPercentile, f_classif
 
-
+class StrToBytes:
+    def __init__(self, fileobj):
+        self.fileobj = fileobj
+    def read(self, size):
+        return self.fileobj.read(size).encode()
+    def readline(self, size=-1):
+        return self.fileobj.readline(size).encode()
 
 def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/email_authors.pkl"):
-    """ 
+    """
         this function takes a pre-made list of email texts (by default word_data.pkl)
         and the corresponding authors (by default email_authors.pkl) and performs
         a number of preprocessing steps:
@@ -30,17 +43,16 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
     ### the words (features) and authors (labels), already largely preprocessed
     ### this preprocessing will be repeated in the text learning mini-project
     authors_file_handler = open(authors_file, "r")
-    authors = pickle.load(authors_file_handler)
+    authors = pickle.load(StrToBytes(authors_file_handler))
     authors_file_handler.close()
 
     words_file_handler = open(words_file, "r")
-    word_data = cPickle.load(words_file_handler)
+    word_data = cPickle.load(StrToBytes(words_file_handler))
     words_file_handler.close()
 
     ### test_size is the percentage of events assigned to the test set
     ### (remainder go into training)
     features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
-
 
 
     ### text vectorization--go from strings to lists of numbers
@@ -51,7 +63,7 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
 
 
 
-    ### feature selection, because text is super high dimensional and 
+    ### feature selection, because text is super high dimensional and
     ### can be really computationally chewy as a result
     selector = SelectPercentile(f_classif, percentile=10)
     selector.fit(features_train_transformed, labels_train)
@@ -59,7 +71,7 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
     features_test_transformed  = selector.transform(features_test_transformed).toarray()
 
     ### info on the data
-    print "no. of Chris training emails:", sum(labels_train)
-    print "no. of Sara training emails:", len(labels_train)-sum(labels_train)
-    
+    print("no. of Chris training emails:", sum(labels_train))
+    print ("no. of Sara training emails:", len(labels_train)-sum(labels_train))
+
     return features_train_transformed, features_test_transformed, labels_train, labels_test
